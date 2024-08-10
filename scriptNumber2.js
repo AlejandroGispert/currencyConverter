e93374c80039dca7485cf1a739bfc0910055f88e
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-08-10 19:56:12 +0200

    injection test2

--- scriptNumber2.js
+++ scriptNumber2.js
@@ -2,7 +2,7 @@ const countriesFrom2 = document.getElementById("countries-from-Select");
 const countriesTo2 = document.getElementById("countries-to-Select");
 const suggestions = document.getElementById("suggestions");
 
-const elToqueFetchedData = [];
+// const elToqueFetchedData = [];
 async function fetchElToque() {
   const url3 = "https://currency-backend.netlify.app/.netlify/functions/toque";
 
@@ -12,19 +12,19 @@ async function fetchElToque() {
     if (!response.ok) {
       throw new Error("Network response was not ok");
     } else {
-      const cupInputText = `<option value="CUP">
-                  Cuban Peso
-                  <span
-                    class="yellow-star"
-                    style="font-size: 24px; color: yellow"
-                    >&#9733;</span
-                  >
-                </option>`;
+      // const cupInputText = `<option value="CUP">
+      //             Cuban Peso
+      //             <span
+      //               class="yellow-star"
+      //               style="font-size: 24px; color: yellow"
+      //               >&#9733;</span
+      //             >
+      //           </option>`;
 
-      const cupOptionText = `<option value="CUP">$</option>`;
-      countriesFrom2.prepend(cupInputText);
-      countriesTo2.prepend(cupInputText);
-      suggestions.prepend(cupOptionText);
+      // const cupOptionText = `<option value="CUP">$</option>`;
+      // countriesFrom2.prepend(cupInputText);
+      // countriesTo2.prepend(cupInputText);
+      // suggestions.prepend(cupOptionText);
 
       const data = await response.json();
       console.log("fetchedData", data.tasas);
