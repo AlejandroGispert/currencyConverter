76f5d7ac01529864748f90615174aa18500740b4
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-08-12 10:20:44 +0200

    injection test7

---
+++ functions/CUPtoUSD.js
@@ -0,0 +1,56 @@
+exports.handler = async function (event, context) {
+  const fetch = (await import("node-fetch")).default;
+
+  const pad = (number) => (number < 10 ? `0${number}` : number);
+
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = pad(today.getMonth() + 1);
+  const day = pad(today.getDate());
+
+  const date_from = `${year}-${month}-${day} 00:00:01`;
+  const date_to = `${year}-${month}-${day} 01:00:01`;
+
+  const eltoqueWebApi = `https://tasas.eltoque.com/v1/trmi?date_from=${encodeURIComponent(
+    date_from
+  )}&date_to=${encodeURIComponent(date_to)}`;
+
+  try {
+    console.log("Fetching from eltoqueWebApi:", eltoqueWebApi);
+
+    const response = await fetch(eltoqueWebApi, {
+      method: "GET",
+
+      headers: {
+        Authorization: `Bearer ${process.env.ELTOQUE_API_KEY}`,
+        Accept: "*/*",
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log("Response received:", response);
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log("Parsed response data:", data);
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        // "Access-Control-Allow-Origin": "https://currency-backend.netlify.app", // Adjust for security
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(data.rates[1]),
+    };
+  } catch (error) {
+    console.error("Fetch error:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to fetch rates" }),
+    };
+  }
+};
