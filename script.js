3b4eedf45d675360fcf8252c81455c98079cfa7b
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2024-08-13 10:42:47 +0200

    test37

--- script.js
+++ script.js
@@ -686,7 +686,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       savedAlert.style.visibility = "visible";
 
       const alertRateDisplayed =
-        e.rate === "CUP" ? "not available" : e.rate.toFixed(3);
+        e.symbolFrom === "CUP" ? "not available" : e.rate.toFixed(3);
 
       savedAlert.style.listStyle = "none";
       savedAlert.innerHTML = `<p style="margin: 0">${e.symbolFrom} to ${e.symbolTo} rates</p><span style="font-size: 10px">then ${alertRateDisplayed},  alert on: ${e.alertOn}</span><button type="button"  class="rem-button" data-index="${index}"  style="width:15px;height:20px;background-color:white;margin-left:15px;padding: 0;float:right;border-radius:5px">x</button>`;
